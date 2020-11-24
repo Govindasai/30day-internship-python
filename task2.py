3791e275974e1f96448f35e008e6b4723744992c
Govindasai <64857545+Govindasai@users.noreply.github.com>
2020-11-24 22:26:58 +0530

    Task2

---
+++ task2.py
@@ -0,0 +1,19 @@
+k="kovinda sai"
+print(k.replace("k","g"))
+
+x=100
+y=60
+z=40
+print(x/10)
+print(y*50)
+print(z+90)
+
+
+a=9
+b=9.0
+print("before conversion")
+print("a = ",a)
+print("b = ",b)
+print("after conversion")
+print("a = ", float(a))
+print("b =",int(b))
