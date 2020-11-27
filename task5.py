e0bc6c27d14cc8ef0da80ae8266cf3acff2f5854
Govindasai <64857545+Govindasai@users.noreply.github.com>
2020-11-27 14:36:54 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def addition(x,y):
+    print('addition of two numbers',(x+y))
+    
+def subtraction(x,y):
+    print('subtraction of two numbers',(x-y))
+
+def multiplication(x,y):
+    print('multiplication of two numbers',(x*y))
+    
+def division(x,y):
+    print('division of two numbers',(x/y))
+    
+    
+x= int(input('enter the value of x'))
+y= int(input('enter the value of y'))
+addition(x,y)
+subtraction(x,y)
+division(x,y)
+multiplication(x,y)
+
+
+# In[4]:
+
+
+def covid(patient_name,body_temp=98):
+    patient_details={'patient_name':name,'body_temp':body_temperature}
+    print(patient_details)
+    
+patient_name=input('enter the patient name')
+body_temperature=int(input('enter the patient body temperature'))
+covid(patient_name,temperature)
+
+
+# In[ ]:
+
+
+
+
